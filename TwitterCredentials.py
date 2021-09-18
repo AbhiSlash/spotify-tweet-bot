e38db9457fe5bc59046de4cfe103d5ed4e4a7ad4
AbhiSlash <abhilash.sadanand@gmail.com>
2021-09-19 04:37:36 +0530

    Update TwitterCredentials.py

--- TwitterCredentials.py
+++ TwitterCredentials.py
@@ -1,5 +1,5 @@
 #The authetication tokens and keys for Twitter can be found when you create an app on https://developer.twitter.com/
-CONSUMER_KEY =""
-CONSUMER_SECRET = "" 
-ACCESS_KEY = ""
-ACCESS_SECRET = ""
+CONSUMER_KEY ="C95gA3qyHMCP6ODvqIZt9W6Gx"
+CONSUMER_SECRET = "0UrpTEIN0hISLS0nClFtUiHzr7vq1EAdojqy3fdDxVRs7SLfjr" 
+ACCESS_KEY = "1269532302058520578-GbPZbpbS8XxiLKu7YMMapTi9GZWiOL"
+ACCESS_SECRET = "qRIW7psBRBtVBTkoHfLhghIdD7I8QwBK6aivNSAwqk2pE"
