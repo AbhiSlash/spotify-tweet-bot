19d3d4341c046e38da074aa8eb843915d29703df
AbhiSlash <abhilash.sadanand@gmail.com>
2021-09-19 04:33:40 +0530

    Update SpotifyCredentials.py

--- SpotifyCredentials.py
+++ SpotifyCredentials.py
@@ -1,6 +1,6 @@
 #The authentication tokens and keys for Spotify can be found when you create an app on https://developer.spotify.com/
-CLIENT_ID = ""
-CLIENT_SECRET = ""
+CLIENT_ID = "26d6e24cfe3c424c8c242a45f650b203"
+CLIENT_SECRET = "8277ad6182a349ffb0da42c10cd1f0a0"
 scope = 'user-read-currently-playing'
-username = ""
-redirect_uri = ""
+username = "22kdq76fqqqbzm5gobkk4t6ua"
+redirect_uri = "https://spotifybotderptag.herokuapp.com/"
